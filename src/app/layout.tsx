6212eb6a5f6974c438baa45e335ca25a58005542
Ven Gist <vengist@odyssy.io>
2025-03-05 12:00:16 -0800

    feat: Change default theme to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+        >
           <Hamburger />
           {children}
         </ThemeProvider>
