4618bb9b69866d94c07c27163f6d60fc8b486d92
Ven Gist <vengist@odyssy.io>
2025-03-20 17:26:26 -0700

    Update Discord link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             </a>
             <a
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[rgb(var(--foreground-rgb))] gap-2 text-accent text-[rgb(var(--foreground-rgb))] hover:text-[rgb(var(--foreground-rgb))] hover:transition-colors hover:bg-[rgb(var(--foreground-rgb))] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://discord.gg/JRvgcnha"
+              href="https://discord.gg/t6AWuYrp"
               target="_blank"
               rel="noopener noreferrer"
             >
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://discord.gg/JRvgcnha"
+          href="https://discord.gg/t6AWuYrp"
           target="_blank"
           rel="noopener noreferrer"
         >
