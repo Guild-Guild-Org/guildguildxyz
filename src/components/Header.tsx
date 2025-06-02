cd864596c3a3f2a97be5846c2dd5175a1cf5b0f4
Ven Gist <vengist@odyssy.io>
2025-06-02 13:47:54 -0700

    update logo and discord link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import Hamburger from "@/components/Navigation/Hamburger";
 import ThemeToggle from "@/components/ThemeToggle";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <main className="header font-[family-name:var(--font-geist-sans)]">
       <div className="w-full flex flex-row justify-between p-8">
         <Link href="/">
-          <div className="flex flex-row gap-4 items-end">
-            <p className="text-3xl">GG</p>
+          <div className="flex flex-row gap-4 items-center">
+            <Image src="/gg_logo.png" width={48}
+      height={48} alt="gg logo" />
             <p>Guild Guild</p>
           </div>
         </Link>
